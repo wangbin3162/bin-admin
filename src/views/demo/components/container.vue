ca4fecb054504264f5e19173abedbf93d275bab8
wangbin3162 <33084011+wangbin3162@users.noreply.github.com>
2019-07-21 11:45:12 +0800

    优化滚动组件，扩展了固定的头部和尾部，可配置为默认文档路

--- src/views/demo/components/container.vue
+++ src/views/demo/components/container.vue
@@ -1,5 +1,6 @@
 <template>
-  <bin-container ref="container">
+  <bin-container ref="container" header-fixed>
+    <div slot="header">固定头部</div>
     <div class="container-wrap">
       <div class="info">
         <h2>bin-container</h2>
@@ -11,11 +12,14 @@
           <b-button @click="scrollTo300" type="primary">滚动到距离顶部300px</b-button>
           <b-button @click="scrollToDom" type="primary">滚动到第10行内容</b-button>
         </div>
-        <div style="padding: 20px;">
+
+        <p>我们还可以使用 `header-fixed` `footer-fixed`两个属性配合slot 的header footer来给容器增加固定的头和底部，或者是只添加插槽来插入可滚动的头和底部</p>
+        <div style="padding: 20px; background: #eee;">
           <div v-for="item in 50" :key="item" ref="item">{{item}}:这是用来撑开内容的行...</div>
         </div>
       </div>
     </div>
+    <div slot="footer">底部快捷栏(不使用footer-fixed则默认为文档流底部)</div>
   </bin-container>
 </template>
 
@@ -40,6 +44,9 @@
       color: #314659;
       font-size: 14px;
       line-height: 2;
+      h2 {
+        margin: 0;
+      }
       p, pre {
         padding: 12px 20px;
         margin: 16px 0;
