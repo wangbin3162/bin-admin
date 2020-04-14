5c752bd833cc3e038bc299fb0fde27c875380397
wangbin <316281400@qq.com>
2020-04-14 14:04:58 +0800

    表单验证试验模块隐藏

--- src/router/demo/form-demo.js
+++ src/router/demo/form-demo.js
@@ -4,13 +4,13 @@ export default {
   path: '/formDemo',
   component: layout,
   redirect: { name: 'FormCfg' },
-  meta: { title: '表单配置', icon: 'ios-today', roles: true },
+  meta: { title: '表单配置', icon: 'ios-today', roles: false },
   children: [
     {
       path: 'formCfg',
       name: 'FormCfg',
       component: () => import('../../pages/demo/form/FormDemo'),
-      meta: { title: '信息项配置(实验)', roles: true }
+      meta: { title: '信息项配置(实验)', roles: false }
     }
   ]
 }
