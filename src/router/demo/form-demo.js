4b6f1c609acb435ef31afe98ddc90ad8b9c6cd75
wangbin <316281400@qq.com>
2020-05-20 15:47:43 +0800

    我的采集新页面

--- src/router/demo/form-demo.js
+++ src/router/demo/form-demo.js
@@ -13,10 +13,16 @@ export default {
       meta: { title: '动态表单(测试)', roles: false }
     },
     {
-      path: 'ResInfo2',
+      path: 'resInfo2',
       name: 'ResInfo2',
       component: () => import('../../pages/data-manage/res/ResInfo2'),
       meta: { title: '资源信息(测试)', roles: true }
+    },
+    {
+      path: 'gather2',
+      name: 'Gather2',
+      component: () => import('../../pages/data-manage/data-exchange/Gather2'),
+      meta: { title: '我的采集(新)', roles: true }
     }
   ]
 }
