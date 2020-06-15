e27d052ecb15c1609be34b74ae10b86c4f24de26
haodongdong <dongdong_hao@jsxzkingdee.com>
2020-06-15 09:41:42 +0800

    [updated] 信用异议标准样式

--- src/pages/credit-service/credit-diff/diff-approve/Audit.vue
+++ src/pages/credit-service/credit-diff/diff-approve/Audit.vue
@@ -1,87 +1,81 @@
 <template>
   <div class="diff-app-detail">
     <page-header-wrap show-close @on-close="$emit('close')">
-      <v-edit-wrap>
-        <div slot="full">
-          <b-collapse :value="['1', '2']" simple>
-            <b-collapse-panel title="异议申请详情信息" name="1">
-              <table class="table">
-                <tr>
-                  <td>异议编码：</td>
-                  <td>{{ detail.searchCode }}</td>
-                  <td>异议申请人：</td>
-                  <td>{{ detail.applyUserName }}</td>
-                </tr>
-                <tr>
-                  <td>申请人身份证号码：</td>
-                  <td>{{ detail.idCode }}</td>
-                  <td>联系电话：</td>
-                  <td>{{ detail.phone }}</td>
-                </tr>
-                <tr>
-                  <td>数据类型：</td>
-                  <td>
-                    <b-button type="text" @click="handleOpenResDetail">
-                      {{ detail.resourceName }}
-                    </b-button>
-                  </td>
-                  <td>异议申请时间：</td>
-                  <td>{{ detail.applyDate }}</td>
-                </tr>
-                 <tr>
-                  <td>数源单位：</td>
-                  <td>{{ detail.sourceDeptName }}</td>
-                </tr>
-                <tr>
-                  <td>异议内容：</td>
-                  <td>{{ detail.applyDescription }}</td>
-                </tr>
-                <tr>
-                  <td colspan="4">
-                    <b-divider></b-divider>
-                  </td>
-                </tr>
-                <tr>
-                  <td colspan="4">
-                    <h4>附件信息</h4>
-                  </td>
-                </tr>
-                <tr v-for="item in detail.attachments" :key="item.id">
-                  <td colspan="2" >
-                    <div flex="main:justify">
-                      <span>{{ item.fileName }}</span>
-                      <attach-dl-btn
-                        :id="item.id"
-                        :fileName="item.fileName">
-                      </attach-dl-btn>
-                    </div>
-                  </td>
-                </tr>
-              </table>
-            </b-collapse-panel>
+      <v-edit-wrap transparent>
+        <b-collapse-wrap title="异议申请详情信息" collapse>
+          <table class="table">
+            <tr>
+              <td>异议编码：</td>
+              <td>{{ detail.searchCode }}</td>
+              <td>异议申请人：</td>
+              <td>{{ detail.applyUserName }}</td>
+            </tr>
+            <tr>
+              <td>申请人身份证号码：</td>
+              <td>{{ detail.idCode }}</td>
+              <td>联系电话：</td>
+              <td>{{ detail.phone }}</td>
+            </tr>
+            <tr>
+              <td>数据类型：</td>
+              <td>
+                <b-button type="text" @click="handleOpenResDetail">
+                  {{ detail.resourceName }}
+                </b-button>
+              </td>
+              <td>异议申请时间：</td>
+              <td>{{ detail.applyDate }}</td>
+            </tr>
+              <tr>
+              <td>数源单位：</td>
+              <td>{{ detail.sourceDeptName }}</td>
+            </tr>
+            <tr>
+              <td>异议内容：</td>
+              <td>{{ detail.applyDescription }}</td>
+            </tr>
+            <tr>
+              <td colspan="4">
+                <b-divider></b-divider>
+              </td>
+            </tr>
+            <tr>
+              <td colspan="4">
+                <h4>附件信息</h4>
+              </td>
+            </tr>
+            <tr v-for="item in detail.attachments" :key="item.id">
+              <td colspan="2" >
+                <div flex="main:justify">
+                  <span>{{ item.fileName }}</span>
+                  <attach-dl-btn
+                    :id="item.id"
+                    :fileName="item.fileName">
+                  </attach-dl-btn>
+                </div>
+              </td>
+            </tr>
+          </table>
+        </b-collapse-wrap>
 
-            <div class="line"></div>
-
-            <b-collapse-panel title="初审结果" name="2">
-              <b-form ref="form" :model="form" :rules="rules" :label-width="100">
-                <b-form-item label="初审结果" prop="status">
-                  <b-radio-group v-model="form.status">
-                    <b-radio label="T1">初审通过</b-radio>
-                    <b-radio label="TR">驳回</b-radio>
-                  </b-radio-group>
-                </b-form-item>
-                <b-form-item label="初审说明" prop="approveDescription">
-                  <b-input v-model="form.approveDescription" type="textarea" :rows="4" placeholder="请输入...." ></b-input>
-                </b-form-item>
-                <b-form-item>
-                  <b-button type="primary" @click="handleSubmit">
-                    初审完成
-                  </b-button>
-                </b-form-item>
-              </b-form>
-            </b-collapse-panel>
-          </b-collapse>
-        </div>
+        <b-collapse-wrap title="初审结果" collapse>
+          <b-form ref="form" :model="form" :rules="rules" :label-width="100">
+            <b-form-item label="初审结果" prop="status">
+              <b-radio-group v-model="form.status">
+                <b-radio label="T1">初审通过</b-radio>
+                <b-radio label="TR">驳回</b-radio>
+              </b-radio-group>
+            </b-form-item>
+            <b-form-item label="初审说明" prop="approveDescription">
+              <b-input v-model="form.approveDescription" type="textarea" :rows="4" placeholder="请输入...." ></b-input>
+            </b-form-item>
+            <b-form-item>
+              <b-button type="primary" @click="handleSubmit">
+                初审完成
+              </b-button>
+            </b-form-item>
+          </b-form>
+        </b-collapse-wrap>
 
         <template slot="footer">
           <b-button @click="$emit('close')">返 回</b-button>
