f466c31569408cdfe6c23a2374cbe45aeac86e1b
haodongdong <dongdong_hao@jsxzkingdee.com>
2020-06-25 11:33:52 +0800

    [updated] 添加获取时间范围通用函数

--- src/common/utils/util.js
+++ src/common/utils/util.js
@@ -67,3 +67,69 @@ export function enumToOptions (enumData) {
   }
   return options
 }
+
+/**
+ * @author haodongdong
+ * @description 获取时间范围
+ * @param {number} days 间隔时间
+ * @param {string} [mode = {y}-{m}] 定义返回的日期字符串格式，默认返回yyyy-mm形式
+ * @returns {
+   {
+     startDate: Date,
+     endDate: Date,
+     startDateStr: string,
+     endDateStr: string
+   }
+ } Object
+ */
+export function getTimeRange(days, mode = '{y}-{m}') {
+  const startDate = new Date()
+  const endDate = new Date()
+  startDate.setTime(startDate.getTime() - 3600 * 1000 * 24 * days)
+
+  let parseTime = function (time, cFormat) {
+    if (arguments.length === 0) {
+      return null
+    }
+    const format = cFormat || '{y}-{m}-{d} {h}:{i}:{s}'
+    let date
+    if (typeof time === 'object') {
+      date = time
+    } else {
+      if (('' + time).length === 10) time = parseInt(time) * 1000
+      date = new Date(time)
+    }
+    const formatObj = {
+      y: date.getFullYear(),
+      m: date.getMonth() + 1,
+      d: date.getDate(),
+      h: date.getHours(),
+      i: date.getMinutes(),
+      s: date.getSeconds(),
+      a: date.getDay()
+    }
+    // eslint-disable-next-line
+    const time_str = format.replace(/{(y|m|d|h|i|s|a)+}/g, (result, key) => {
+      let value = formatObj[key]
+      if (key === 'a') return ['一', '二', '三', '四', '五', '六', '日'][value - 1]
+      if (result.length > 0 && value < 10) {
+        value = '0' + value
+      }
+      return value || 0
+    })
+    // eslint-disable-next-line
+    return time_str
+  }
+
+  const startDateStr = parseTime(startDate, mode)
+  const endDateStr = parseTime(endDate, mode)
+
+  parseTime = null
+
+  return {
+    startDate,
+    endDate,
+    startDateStr,
+    endDateStr
+  }
+}
