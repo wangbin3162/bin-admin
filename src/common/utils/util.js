1af96000bc9cc7301287b5b1e5dc5b3c4fe3c3f3
haodongdong <dongdong_hao@jsxzkingdee.com>
2020-06-25 11:43:42 +0800

    [updated] 时间解析函数用bin-ui引入

--- src/common/utils/util.js
+++ src/common/utils/util.js
@@ -1,4 +1,5 @@
 import cookies from './util.cookies'
+import binUtil from 'bin-ui/src/utils/util'
 
 const util = {
   cookies
@@ -87,44 +88,8 @@ export function getTimeRange(days, mode = '{y}-{m}') {
   const endDate = new Date()
   startDate.setTime(startDate.getTime() - 3600 * 1000 * 24 * days)
 
-  let parseTime = function (time, cFormat) {
-    if (arguments.length === 0) {
-      return null
-    }
-    const format = cFormat || '{y}-{m}-{d} {h}:{i}:{s}'
-    let date
-    if (typeof time === 'object') {
-      date = time
-    } else {
-      if (('' + time).length === 10) time = parseInt(time) * 1000
-      date = new Date(time)
-    }
-    const formatObj = {
-      y: date.getFullYear(),
-      m: date.getMonth() + 1,
-      d: date.getDate(),
-      h: date.getHours(),
-      i: date.getMinutes(),
-      s: date.getSeconds(),
-      a: date.getDay()
-    }
-    // eslint-disable-next-line
-    const time_str = format.replace(/{(y|m|d|h|i|s|a)+}/g, (result, key) => {
-      let value = formatObj[key]
-      if (key === 'a') return ['一', '二', '三', '四', '五', '六', '日'][value - 1]
-      if (result.length > 0 && value < 10) {
-        value = '0' + value
-      }
-      return value || 0
-    })
-    // eslint-disable-next-line
-    return time_str
-  }
-
-  const startDateStr = parseTime(startDate, mode)
-  const endDateStr = parseTime(endDate, mode)
-
-  parseTime = null
+  const startDateStr = binUtil.parseTime(startDate, mode)
+  const endDateStr = binUtil.parseTime(endDate, mode)
 
   return {
     startDate,
