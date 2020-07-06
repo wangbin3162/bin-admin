6459e160e6bb12acbf283d08ffd3aa0f233f38ca
haodongdong <dongdong_hao@jsxzkingdee.com>
2020-07-06 11:43:32 +0800

    [updated] 信用服务--->信用异议--->异议申请，详情样式调整

--- src/pages/credit-service/credit-diff/diff-app/Detail.vue
+++ src/pages/credit-service/credit-diff/diff-app/Detail.vue
@@ -5,51 +5,44 @@
         <b-collapse-wrap title="异议处理流程" collapse>
           <table class="table">
             <tr>
-              <td>主体名称：</td>
+              <td>主体名称</td>
               <td>{{ detail.name }}</td>
-              <td>数源单位：</td>
+              <td>数源单位</td>
               <td>{{ detail.sourceDeptName }}</td>
             </tr>
             <tr>
-              <td>资源标识符：</td>
+              <td>资源标识符</td>
               <td>{{ detail.resourceKey }}</td>
-              <td>数据类型：</td>
+              <td>数据类型</td>
               <td>{{ detail.resourceName }}</td>
             </tr>
             <tr>
-              <td>申请人姓名：</td>
+              <td>申请人姓名</td>
               <td>{{ detail.applyUserName }}</td>
-              <td>申请人身份证号码：</td>
+              <td>申请人身份证号码</td>
               <td>{{ detail.idCode }}</td>
             </tr>
               <tr>
-              <td>申请人电话号码：</td>
+              <td>申请人电话号码</td>
               <td>{{ detail.phone }}</td>
-              <td>申请人电子邮箱：</td>
+              <td>申请人电子邮箱</td>
               <td>{{ detail.email }}</td>
             </tr>
               <tr>
-              <td>申请时间：</td>
+              <td>申请时间</td>
               <td>{{ detail.applyDate }}</td>
-              <td>流程状态：</td>
+              <td>流程状态</td>
               <td>{{ detail.status }}</td>
             </tr>
             <tr>
-              <td>内容描述：</td>
-              <td>{{ detail.applyDescription }}</td>
+              <td>内容描述</td>
+              <td colspan="3">{{ detail.applyDescription }}</td>
             </tr>
-            <tr>
-              <td colspan="4">
-                <b-divider></b-divider>
+            <tr v-for="(item, index) in detail.attachments" :key="item.id">
+              <td v-if="index === 0" :rowspan="detail.attachments.length + 1">
+                附件信息
               </td>
-            </tr>
-            <tr>
-              <td colspan="4">
-                <h4>附件信息</h4>
-              </td>
-            </tr>
-            <tr v-for="item in detail.attachments" :key="item.id">
-              <td colspan="2" >
+              <td colspan="3" style="padding: 5px 5px 5px 10px; background: #ffffff;">
                 <div flex="main:justify">
                   <span>{{ item.fileName }}</span>
                   <attach-dl-btn
@@ -65,14 +58,14 @@
         <b-collapse-wrap title="初审概况" collapse>
           <table class="table">
             <tr>
-              <td>初审人姓名：</td>
+              <td>初审人姓名</td>
               <td>{{ detail.approveUserName }}</td>
-              <td>初审时间：</td>
+              <td>初审时间</td>
               <td>{{ detail.approveDate }}</td>
             </tr>
             <tr>
-              <td>初审说明：</td>
-              <td>{{ detail.approveDescription }}</td>
+              <td>初审说明</td>
+              <td colspan="3">{{ detail.approveDescription }}</td>
             </tr>
           </table>
         </b-collapse-wrap>
@@ -80,18 +73,18 @@
         <b-collapse-wrap title="处理结果" collapse>
           <table class="table">
             <tr class="cust">
-              <td>处理人姓名：</td>
+              <td>处理人姓名</td>
               <td>{{ detail.dealUserName }}</td>
-              <td>处理时间：</td>
+              <td>处理时间</td>
               <td>{{ detail.dealDate }}</td>
             </tr>
             <tr>
-              <td>处理结果：</td>
-              <td>{{ detail.dealResult }}</td>
+              <td>处理结果</td>
+              <td colspan="3">{{ detail.dealResult }}</td>
             </tr>
             <tr>
-              <td>处理说明：</td>
-              <td>{{ detail.dealDescription }}</td>
+              <td>处理说明</td>
+              <td colspan="3">{{ detail.dealDescription }}</td>
             </tr>
           </table>
         </b-collapse-wrap>
@@ -142,15 +135,43 @@
   }
 
   .table {
-    width: 100%;
-    font-size: 13px;
     border-collapse: collapse;
+    margin: 0 auto;
+    text-align: center;
+    width: 100%;
 
-    td {
+    td, th {
+      border: 1px solid #cad9ea;
+      border: 1px solid #e8eaec;
+      color: #666;
       height: 40px;
-      width: 25%;
-      padding: 5px 12px
     }
+    td:nth-child(odd), th:nth-child(odd) {
+      width: 20%;
+      padding-right: 15px;
+      text-align: right;
+      background: #f5fafa;
+      background: #fafafa;
+    }
+    td:nth-child(even), th:nth-child(even) {
+      width: 30%;
+      padding: 5px;
+      padding-left: 10px;
+      text-align: left;
+      background: #ffffff;
+    }
+    tr:hover {
+      td {
+        background: #f4f5f6;
+        transition: background 0.7s;
+      }
+    }
+    // tr:nth-child(odd) {
+    //   background: #f5fafa;
+    // }
+    // tr:nth-child(even) {
+    //   background: #ffffff;
+    // }
   }
 }
 </style>
