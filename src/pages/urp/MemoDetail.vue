f9005b47fcdc8a0d3a0e7b15bc0245267168e4bf
wangbin <316281400@qq.com>
2020-07-06 17:45:27 +0800

    初步实现备忘录详情

--- src/pages/urp/MemoDetail.vue
+++ src/pages/urp/MemoDetail.vue
@@ -8,8 +8,54 @@
           <b-breadcrumb-item :to="{ path: '/urp/memoList' }">联合奖惩备忘录</b-breadcrumb-item>
           <b-breadcrumb-item>备忘录详情</b-breadcrumb-item>
         </b-breadcrumb>
-        <p>备忘录详情(接口暂未提供)</p>
-        <p>{{ detail }}</p>
+        <div class="result-detail-box">
+          <div class="top-detail" v-if="detail.memo">
+            <div class="icon">
+              <img src="../../assets/images/urp/bwl.png" alt=""/>
+            </div>
+            <div class="info">
+              <h2 class="mb-10">{{ detail.memo.memoName }}</h2>
+              <div flex="box:mean">
+                <simple-label label="备忘录类型">{{ memoTypeMap[ detail.memo.memoType ] }}</simple-label>
+                <simple-label label="备忘录状态">{{ memoStatusMap[ detail.memo.memoStatus ] }}</simple-label>
+                <simple-label label="文号">{{ detail.memo.fileCode }}</simple-label>
+              </div>
+              <div flex="box:mean">
+                <simple-label label="签署日期">{{ detail.memo.signDate }}</simple-label>
+                <simple-label label="联合部门数">{{ detail.memo.unionNum }}</simple-label>
+                <simple-label label="创建时间">{{ detail.memo.createDate }}</simple-label>
+              </div>
+            </div>
+          </div>
+          <urp-left-right>
+            <div slot="left-header">参与部门 <span style="color:#999;">({{ detail.departs.length }})</span></div>
+            <div slot="right-header">处置措施</div>
+            <div slot="left">
+              <b-timeline>
+                <b-timeline-item v-for="dept in detail.departs" :key="dept.id"
+                                 :color="dept.id===activeDeptID?'primary':'info'">
+                  <span @click="handleSelectDept(dept.id)"
+                        :style="{cursor: 'pointer',color:dept.id===activeDeptID?'#0d85ff':'#35495e'}">
+                    {{ dept.name }}
+                  </span>
+                </b-timeline-item>
+              </b-timeline>
+            </div>
+            <div slot="right">
+              <div class="departs-item" v-for="dept in detail.departs" :key="dept.id">
+                <div class="dept-name" :class="{'active':dept.id===activeDeptID}"
+                     :id="dept.id">
+                  {{ dept.name }}
+                </div>
+                <div class="measure-item" v-for="measure in dept.measures" :key="measure.id">
+                  <b-icon name="ios-checkbox"/>
+                  <span>{{ measure.measureName }}</span>
+                </div>
+              </div>
+            </div>
+          </urp-left-right>
+          {{ detail }}
+        </div>
       </div>
     </div>
   </urp-layout>
@@ -20,14 +66,19 @@
   import UrpLayout from './components/UrpLayout'
   import UrpHeader from './components/UrpHeader'
   import { getMemoDetail } from '../../api/urp.api'
+  import UrpLeftRight from './components/UrpLeftRight'
 
   export default {
     name: 'MemoDetail',
-    components: { UrpHeader, UrpLayout },
+    components: { UrpLeftRight, UrpHeader, UrpLayout },
     data() {
       return {
         memoId: '',
-        detail: null
+        detail: null,
+        memoTypeMap: { '1': '惩戒', '2': '激励' },
+        memoTypeStyleMap: { '1': 'primary', '2': 'danger' },
+        memoStatusMap: { '0': '通报', '1': '实施', '2': '过期' },
+        activeDeptID: ''
       }
     },
     created() {
@@ -57,12 +108,19 @@
       },
       // 查询详情
       searchDetail() {
-        console.log('查询详情', this.memoId)
+        this.activeDeptID = ''
         getMemoDetail(this.memoId).then(resp => {
           if (resp.data.code === '0') {
             this.detail = resp.data.data
+            if (this.detail.departs && this.detail.departs.length > 0) {
+              this.activeDeptID = this.detail.departs[0].id
+            }
           }
         })
+      },
+      // 点击选中左侧一个部门
+      handleSelectDept(id) {
+        this.activeDeptID = id
       }
     }
   }
---
+++ src/pages/urp/components/UrpLeftRight.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="left-right-wrapper">
+    <div class="left-panel">
+      <div class="top">
+        <title-bar tip-pos="left" :font-size="16">
+          <slot name="left-header">左边标题</slot>
+        </title-bar>
+      </div>
+      <div class="panel">
+        <slot name="left">left</slot>
+      </div>
+    </div>
+    <div class="right-panel">
+      <div class="top">
+        <title-bar tip-pos="left" :font-size="16">
+          <slot name="right-header">右边标题</slot>
+        </title-bar>
+      </div>
+      <div class="panel">
+        <slot name="right">right</slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'UrpLeftRight'
+  }
+</script>
+
+<style scoped lang="stylus">
+  .left-right-wrapper {
+    background: #fff;
+    display: flex;
+    border: 1px solid #eeeeee;
+    .left-panel {
+      width: 220px;
+      border-right: 1px solid #eeeeee;
+    }
+    .right-panel {
+      width: calc(100% - 220px);
+    }
+    .top {
+      border-bottom: 1px solid #eeeeee;
+    }
+    .panel {
+      padding: 16px 24px;
+      height: 500px;
+      overflow: auto;
+    }
+  }
+</style>
