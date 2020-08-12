25d2214e944c7ebfc6fab0bf6163a7975cc4d88e
Astlvk <astlvk@vip.qq.com>
2020-08-12 16:16:20 +0800

    [added] 添加主体监管页面文件

--- src/router/credit-supervision.js
+++ src/router/credit-supervision.js
@@ -4,5 +4,11 @@ export default [
     name: 'CreditSupervision',
     component: () => import(/* webpackChunkName: "CreditSupervision" */ '@/pages/credit-supervision/home'),
     meta: { title: '信用监管' }
+  },
+  {
+    path: '/mySupervision',
+    name: 'MySupervision',
+    component: () => import(/* webpackChunkName: "MySupervision" */ '@/pages/credit-supervision/my-supervision'),
+    meta: { title: '我的监管' }
   }
 ]
